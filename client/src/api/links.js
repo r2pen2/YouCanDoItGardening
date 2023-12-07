a40c480ea2168c211012a56112f976c76dd923da
r2pen2 <joedobbelaar@gmail.com>
2023-12-07 17:54:50 -0500

    updated mailing list link

--- client/src/api/links.js
+++ client/src/api/links.js
@@ -3,7 +3,7 @@ export const youtubeLink = "https://www.youtube.com/@youcandoitgardening";
 export const tiktokLink = "https://www.tiktok.com/@youcandoitgardening";
 export const facebookLink = "https://www.facebook.com/youcandoitgardencoaching";
 
-export const mailingListLink = "https://forms.gle/Cx6T1sJ9d4JA9cEPA";
+export const mailingListLink = "https://docs.google.com/forms/d/e/1FAIpQLSfyroVy6fF6ndwOwNYaYxgG1UpSQDRZsQSTknNXWp0OOShB-Q/viewform?usp=sf_link";
 export const contactMeLink = "https://sites.google.com/view/youcandoitgardening/contact-me-by-phone";
 
 export const scheduleInPersonLink = "https://docs.google.com/forms/d/e/1FAIpQLSdRKfkhQgWNoDH7D6POnLhlApjlH82puI6B02BjNTlMcGbjIA/viewform";
